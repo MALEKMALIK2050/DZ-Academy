f2a603b0cd3e610477b52f26f1f847aa43b4001a
MALEKMALIK2050 <malekmalik2030@outlook.fr>
2026-06-06 11:09:13 +0100

    fix: prisma typescript type error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis;
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient({
+const prisma = globalForPrisma.prisma || new PrismaClient({
   log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
 });
 
